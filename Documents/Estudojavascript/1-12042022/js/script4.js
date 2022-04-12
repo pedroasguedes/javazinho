653facbcc62a38c52c2e147fa9b35f2269447bd7
PEDRO AUGUSTO DA SILVA GUEDES <pedro.asguedes@senacsp.edu.br>
2022-04-12 17:26:06 -0300

    criação dos arquivos js e html

--- Documents/Estudojavascript/1-12042022/js/script4.js
+++ Documents/Estudojavascript/1-12042022/js/script4.js
@@ -3,7 +3,14 @@ o script deve receber um número,processar e exibir uma
 mensagem dizendo se o número digitando e par ou impar
 */
 
-
+var n = prompt("Digite um numero");
+var total=n/2;
+if(n%2 == 0){
+	alert("Par");
+}else{
+	alert("Impar");
+}
+alert(total)
 
 
 
