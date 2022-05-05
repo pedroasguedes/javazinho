92f5d75e3ebe554399cd9ef391dcfcb0d66bbc3a
PEDRO AUGUSTO DA SILVA GUEDES <pedro.asguedes@senacsp.edu.br>
2022-05-05 16:24:12 -0300

    quase fim  do trabalho

--- Documents/site-trabalho/js/scripts.js
+++ Documents/site-trabalho/js/scripts.js
@@ -1,13 +1,21 @@
-const button = document.querySelector(`button`)
-const popup= document.querySelector(`.popup-wrapper`)
+function premium(){
+    document.getElementById("menu-premium").style.display="block";
+}
+function fecharpremium(){
+    document.getElementById("menu-premium").style.display="none";
+}
+function contato(){
+    document.getElementById("pag-contato").style.display="block"; 
+}
+function fecharcontato(){
+    document.getElementById("pag-contato").style.display="none"; 
+}
+function login(){
+    document.getElementById("pag-login").style.display="block";
+}
 
-button.addEventListener('click', ()=>{
-    popup.style.display = 'block'
-})
+function fecharlogin(){
+    document.getElementById("pag-login").style.display="none"; 
+}
 
-popup.addEventListener('click',event =>{
-    const classnameofclickedelement = event.target.classlist
-    console.log(classnameofclickedelement)
-    popup.style.display = 'none'
-})
 
