9044b40787f892229a6a511d99e4efdefb07beb7
PEDRO AUGUSTO DA SILVA GUEDES <pedro.asguedes@senacsp.edu.br>
2022-05-12 15:32:35 -0300

    MUdando o estado inicial

---
+++ Documents/EstudoReact/terceiro/src/App.js
@@ -0,0 +1,14 @@
+import {useState} from "react";
+function App() {
+const [numero,setNumero] = useState(1);
+
+return(
+<div> 
+<p>O número atual é {numero}</p>
+<p>
+  <button title="Clique" onClick={() => setNumero(numero + 1)}>Clique Aqui</button>
+</p>
+</div>
+);
+}
+export default App;
