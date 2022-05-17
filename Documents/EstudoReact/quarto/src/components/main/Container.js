99220a600db18d7bd609f397c911ae5918d6ff6b
PEDRO AUGUSTO DA SILVA GUEDES <pedro.asguedes@senacsp.edu.br>
2022-05-17 17:21:29 -0300

    Integração com server

--- Documents/EstudoReact/quarto/src/components/main/Container.js
+++ Documents/EstudoReact/quarto/src/components/main/Container.js
@@ -1,12 +1,40 @@
+import {useState,useEffect} from "react";
+
 import Mensagem from "./Mensagem";
 import Conteudo from "./Conteudo";
 
 export default function Container(){
+
+// vamos o estado inicial dos produtos,ou seja,
+//quais dados existem dentro do objeto produtos
+const [produtos, setProduto] = useState([ 
+    {
+id:"",
+nome:"",
+descricao:"",
+categoria:"",
+preco:"",
+msg:[]
+}
+]);
+
+useEffect(()=>{
+
+fetch("http://10.26.49.54:4000/api/services/produto/listar")
+.then((response)=>response.json())
+.then((dados)=>{
+    setProduto(dados.output);
+})
+.catch((erro)=>console.error(`Erro ao carregar a api ->${erro}`))
+
+},[])
+
+
     return(
         <div className="container">
 
 <Mensagem/>
-<Conteudo/>
+<Conteudo dados = {produtos}/>
 </div>
     );
 }
